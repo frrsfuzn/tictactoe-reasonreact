2d852c68b69dee7a9f03e59080247fbd24f46ff5
frrsfuzn <mochfarrasfauzan@gmail.com>
2022-02-05 20:57:09 +0700

    Make square interactable

--- src/Square.bs.js
+++ src/Square.bs.js
@@ -1,12 +1,21 @@
 'use strict';
 
+var Curry = require("bs-platform/lib/js/curry.js");
 var React = require("react");
 
 function Square(Props) {
-  var value = Props.value;
+  var match = React.useState(function () {
+        return "";
+      });
+  var setValue = match[1];
   return React.createElement("button", {
-              className: "square"
-            }, value);
+              className: "square",
+              onClick: (function (param) {
+                  return Curry._1(setValue, (function (param) {
+                                return "X";
+                              }));
+                })
+            }, match[0]);
 }
 
 var make = Square;
--- src/Square.re
+++ src/Square.re
@@ -1,6 +1,9 @@
 [@react.component]
 
-let make = (~value) => 
-    <button className="square">
+let make = (~value) => {
+    let (value, setValue) = React.useState(_ => "");
+
+    <button className="square" onClick={_ => setValue(_ => "X")}>
         {ReasonReact.string(value)}
     </button>
+}
